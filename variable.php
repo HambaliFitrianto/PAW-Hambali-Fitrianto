7a6616894b8e5a136a19b7c8d39aec45b2f12ba7
HambaliFitrianto <balcoy823@gmail.com>
2022-03-09 22:31:52 +0700

    add file variable.php

---
+++ variable.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variable</title>
+    <style>
+        body {
+            text-align: center;
+            background-color: lightblue;
+        }
+    </style>
+</head>
+<body>
+    <h1>Tugas 2</h1>
+    <p><b>Soal Nomer 2</b></p>
+    <?php
+    $a = 5;
+    $b = 10;
+    $c = 15;
+    $d = 20;
+    $e = 25;
+    $f = 30;
+    $g = 35;
+
+    //penjumlahan
+    $hasilPenjumlahan = $g + $e + $f +$b;
+    echo "hasil penjumlahan : $hasilPenjumlahan </br>"; //hasil penjulahan
+
+    //pengurangan
+    $hasilPengurangan = $f - $f;
+    echo "hasil pengurangan : $hasilPengurangan </br>"; //hasil pengurangan
+
+    //perkalian
+    $hasilPerkalian = $d * $a;
+    echo "hasil perkalian : $hasilPerkalian </br>"; //hasil perkalian
+    ?>
+</body>
+</html>
