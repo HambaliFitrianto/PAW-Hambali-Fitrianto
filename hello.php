602832ff79b3486dcba75ab445f5f55ea3dc8949
HambaliFitrianto <balcoy823@gmail.com>
2022-03-09 22:31:03 +0700

    add file hello.php

---
+++ hello.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello</title>
+    <style>
+        body {
+            text-align: center;
+            background-color: lightblue;
+        }
+    </style>
+</head>
+<body>
+    <h1>Tugas 2</h1>
+    <p><b>Soal Nomer 1</b></p>
+    <?php 
+    //nama saya
+    echo "Nama : Hambali Fitrianto</br></br>";
+    //nim saya
+    echo "Nim : 200411100074";
+    ?>
+</body>
+</html>
